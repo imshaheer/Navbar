ef28293029cfe5b62cb4a4de7c4e8f1324aca39a
Muhammad Shaheer <106494567+imshaheer@users.noreply.github.com>
2023-05-25 04:51:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle nav links on small screens
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('nav-active');
+  burger.classList.toggle('toggle');
+});
+
